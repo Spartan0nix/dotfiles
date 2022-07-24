af973ef6dd399ecd5a5375fd1ba1ccba53992417
Spartan0nix <l.humblot74@gmail.com>
2022-07-24 22:45:00 +0200

    Add command to update 'community.general' ansible module

--- install.sh
+++ install.sh
@@ -9,4 +9,8 @@ sudo apt update && sudo apt upgrade -y
 # Install ansible
 sudo apt install ansible -y
 
+# Update community general module
+ansible-galaxy collection install community.general
+
+# Run ansible playbook
 ansible-playbook main.yml --ask-become-pass -vvv
