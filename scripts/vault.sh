0d3d84c53fd8f49943f1f331dada883ae671bbe7
Spartan0nix <l.humblot74@gmail.com>
2022-08-07 10:28:16 +0200

    add utils script

---
+++ scripts/vault.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install -y openssl
+
+VAULT_PASSWORD_PATH=$HOME/.config/dotfiles/vault_password.txt
+VAULT="vault.yml"
+
+if [ ! -f $VAULT_PASSWORD_PATH ]; then
+    # Creating the vault password file
+    echo "The file containing the ansible vault password is not present."
+    echo "Starting the creating process..."
+    echo "Enter ansible vault password :"
+    read ansible_vault_password
+
+    mkdir -p $HOME/.config/dotfiles
+    echo "${ansible_vault_password}" > $VAULT_PASSWORD_PATH
+    echo "Ansible Vault password stored in file '\$HOME/.config/dotfiles/vault_password.txt'"
+fi
+
+if [ ! -f $VAULT ]; then
+    # Creating the vault
+    echo "The ansible vault is not present."
+    echo "Starting the creating process..."
+    ansible-vault create $VAULT --vault-password-file $VAULT_PASSWORD_PATH
+    echo "Ansible vault '${VAULT}' created"
+
+    # Add vault entries
+    echo "Decrypting vault..."
+    ansible-vault decrypt vault.yml --vault-password-file $VAULT_PASSWORD_PATH
+
+    echo "Adding values to the vault"
+
+    # Add git_username
+    echo "Enter git username :"
+    read git_username
+    ansible-vault encrypt_string "${git_username}" --name "git_username" --vault-password-file $VAULT_PASSWORD_PATH >> $VAULT
+
+    # Add git_email
+    echo "Enter git email :"
+    read git_email
+    ansible-vault encrypt_string "${git_email}" --name "git_email" --vault-password-file $VAULT_PASSWORD_PATH >> $VAULT
+
+    echo "Encrypting vault..."
+    ansible-vault encrypt vault.yml --vault-password-file $VAULT_PASSWORD_PATH
+else
+    echo "The ansible vault is already present. Skipping ..."
+fi
