d621c18da8dfabb46c21ff4fc2da782d2ba5016e
Spartan0nix <l.humblot74@gmail.com>
2022-08-13 15:45:29 +0200

    [feat]: add module to extract vscode PATH when running WSL

--- library/ansible-dev-module.sh
+++ library/ansible-dev-module.sh
@@ -1,6 +1,11 @@
 ---
 export ANSIBLE_LIBRARY=/mnt/c/Users/Public/Dev/Admin-sys/dotfiles/library/
 ansible -m github_release.py -a 'repo_name=tfsec repo_owner=aquasecurity asset_name=tfsec-linux-amd64' localhost
+python3 github_release.py github_release_args.json 
+---
 
 ---
+export ANSIBLE_LIBRARY=/mnt/c/Users/Public/Dev/Admin-sys/dotfiles/library/
+ansible -m get_vscode_path.py -a 'user=Leo' localhost
 python3 github_release.py github_release_args.json 
+---
