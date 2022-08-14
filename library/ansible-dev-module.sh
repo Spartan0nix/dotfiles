2482468abfe2346f7262684c8b887603a874533f
Spartan0nix <l.humblot74@gmail.com>
2022-08-14 10:59:26 +0200

    [feat]: update gitignore

--- library/ansible-dev-module.sh
+++ library/ansible-dev-module.sh
@@ -1,11 +1,12 @@
 ---
 export ANSIBLE_LIBRARY=/mnt/c/Users/Public/Dev/Admin-sys/dotfiles/library/
-ansible -m github_release.py -a 'repo_name=tfsec repo_owner=aquasecurity asset_name=tfsec-linux-amd64' localhost
+ansible -m github_release.py -a 'repo_name=bottom repo_owner=ClementTsang asset_name=amd64.deb' localhost
+ansible -m github_release.py -a 'repo_name=neovim repo_owner=neovim asset_name=nvim-linux64.deb' localhost
 python3 github_release.py github_release_args.json 
 ---
 
 ---
 export ANSIBLE_LIBRARY=/mnt/c/Users/Public/Dev/Admin-sys/dotfiles/library/
-ansible -m get_vscode_path.py -a 'user=Leo' localhost
+ansible -m get_vscode_path.py localhost
 python3 github_release.py github_release_args.json 
 ---
